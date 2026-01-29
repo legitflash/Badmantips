1d30e32d613218270c88586e34c95bd6397f7427
uduafemhe favour <uduafemhe.c.favour@gmail.com>
2026-01-29 12:37:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1727008710-216c8",
+  "appId": "1:972660230937:web:c70423b7a3b28a8d6f9388",
+  "apiKey": "AIzaSyABqx4DKpF3MgOj7fysotpcMP5YOv_wRNw",
+  "authDomain": "studio-1727008710-216c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "972660230937"
+};
